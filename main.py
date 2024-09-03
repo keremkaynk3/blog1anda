fbffdb8cbc3802fb8734d596f09809764715021b
keremkaynak3 <200316020@ogr.cbu.edu.tr>
2024-09-03 16:47:25 +0300

    Updates
    Pictures that I saved as a BLOB in database can be seen.

--- main.py
+++ main.py
@@ -1,38 +1,10 @@
 import sqlite3 as sql
-from flask import Flask, render_template, request
-import addCountry
-import uPdates
+from flask import Flask, render_template, Response
+import io
 
 app = Flask(__name__)
 
-# SQLite veritabanı bağlantısı
-try:
-    db = sql.connect(r"C:/Users/MR. CAPH/PycharmProjects/blogBiranda/blog.db")
-    cursor = db.cursor()
-
-    # Ana ekranda görünecek olan ülkeleri veritabanından çekiyorum.
-    cursor.execute("SELECT ulke_adi, linkler, baskent, kodu, nufus, para_birimi, yerler, fotolar FROM ulkeler")
-    countries = cursor.fetchall()
-except sql.Error as e:
-    print(f"Veritabanı hatası: {e}")
-    countries = []
-
-finally:
-    if db:
-        db.close()
-
-# index.html ve country.html dosyalarımı main dosyamda çağırıyorum.
-@app.route('/')
-def index():
-    return render_template('index.html', countries=countries)
-
-@app.route('/ptfy')
-def portfolio():
-    return render_template('portfolio.html', countries=countries)
-
-
-@app.route('/visited')
-def visited():
+def get_countries():
     try:
         db = sql.connect(r"C:/Users/MR. CAPH/PycharmProjects/blogBiranda/blog.db")
         cursor = db.cursor()
@@ -44,34 +16,47 @@ def visited():
     finally:
         if db:
             db.close()
+    return countries
 
+@app.route('/')
+def index():
+    countries = get_countries()
+    return render_template('index.html', countries=countries)
+
+@app.route('/ptfy')
+def portfolio():
+    countries = get_countries()
+    return render_template('portfolio.html', countries=countries)
+
+@app.route('/visited')
+def visited():
+    countries = get_countries()
     return render_template('visited.html', countries=countries)
-
 
 @app.route('/country/<int:country_id>')
 def country(country_id):
-    # Seçilen ülkenin bilgilerini alıyorum.
+    countries = get_countries()
     country_info = countries[country_id - 1] if 0 < country_id <= len(countries) else None
-
-    # Eğer ülke bulunamazsa veya veri yoksa, hata mesajı gösteriyorum.
     if not country_info:
         return "Ülke bulunamadı!"
+    return render_template('country.html', country=country_info, country_id=country_id)
 
-    return render_template('country.html', country=country_info)
-
-
-@app.route('/add_data', methods=['GET', 'POST'])
-def add_data():
-    if request.method == 'POST':
-        try:
-            a = uPdates.uPdates()
-            a.add_Country()
-            a.add_Country()
-            return "Yeni veriler başarıyla eklendi!"
-        except Exception as e:
-            return f"Veri ekleme sırasında hata oluştu: {e}"
-    return render_template('add_data.html')
-
+@app.route('/image/<int:country_id>')
+def image(country_id):
+    try:
+        db = sql.connect(r"C:/Users/MR. CAPH/PycharmProjects/blogBiranda/blog.db")
+        cursor = db.cursor()
+        cursor.execute("SELECT fotolar FROM ulkeler WHERE rowid = ?", (country_id,))
+        photo_blob = cursor.fetchone()[0]
+        if photo_blob:
+            return Response(photo_blob, mimetype='image/jpeg')
+        return "Fotoğraf bulunamadı!", 404
+    except sql.Error as e:
+        print(f"Veritabanı hatası: {e}")
+        return "Bir hata oluştu!", 500
+    finally:
+        if db:
+            db.close()
 
 if __name__ == '__main__':
     app.run(debug=True)
